a328fc0ad785a7ca94cb4487016f679be7dbb0f9
anishbhargav9 <anishbhargav9.challa@gmail.com>
2026-01-29 23:34:23 +0530

    Add WebSocket placeholder for Step 1

---
+++ client/websocket.js
@@ -0,0 +1,3 @@
+//I focused only on local drawing. I will add WebSocket logic later first i will test local drawing functionality and then i will integrate WebSocket for real time collaboration
+
+console.log('WebSocket module loaded for local drawing only step-1');

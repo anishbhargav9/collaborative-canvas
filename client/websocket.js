666dc01d41df1f275323955dce4a00acda4f0702
anishbhargav9 <anishbhargav9.challa@gmail.com>
2026-01-30 15:45:37 +0530

    Initialize Socket.io client connection

--- client/websocket.js
+++ client/websocket.js
@@ -2,6 +2,7 @@
 // Step 2: establish WebSocket connection
 
 const socket = io();
+window.socket=socket//exposing the socket for other modules
 
 socket.on('connect', () => {
   console.log('Connected to server:', socket.id);

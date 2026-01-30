c8cb54c8970d1c7823d324757842ce5d5c7b2682
anishbhargav9 <anishbhargav9.challa@gmail.com>
2026-01-30 17:01:43 +0530

    Stored strokes on server and handle global undo

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
+const stateManager = require('./state-manager');
 
 const app = express();
 const server = http.createServer(app);
@@ -17,15 +18,26 @@ io.on('connection', (socket) => {
   console.log('Client connected:', socket.id);
   //it will listen for stroke drawing events from clients
   socket.on('stroke:draw', (stroke) => {
-    console.log('Received stroke from', socket.id);
-
+    //here we add the stroke to the server's authoritative history
+  const savedStroke = stateManager.addStroke(stroke);
+  if (!savedStroke) return;
     // sending  the stroke to all other connected clients so that they can render it on their canvases
-  socket.broadcast.emit('stroke:draw', stroke);
-  });
+  socket.broadcast.emit('stroke:draw', savedStroke);
+});
+
   //handling client disconnection if disconnected it will log the socket id in the console
   socket.on('disconnect', () => {
     console.log('Client disconnected:', socket.id);
   });
+
+
+  //adding a new functionality that is the global undo feature
+  socket.on('action:undo', () => {
+  stateManager.undoLastStroke();//removing the last stroke from the server's history
+  const updatedHistory = stateManager.getStrokeHistory();//getting the updated history after undo
+  io.emit('canvas:sync', updatedHistory);//sending the updated history to all clients to sync their canvases
+});
+
 });
 
 server.listen(PORT, () => {

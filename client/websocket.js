7ec5b00930445ac1196569e89d94a862bdac4c5f
anishbhargav9 <anishbhargav9.challa@gmail.com>
2026-01-30 16:16:50 +0530

    Fix receiving and rendering of remote drawing strokes

--- client/websocket.js
+++ client/websocket.js
@@ -7,6 +7,13 @@ window.socket=socket//exposing the socket for other modules
 socket.on('connect', () => {
   console.log('Connected to server:', socket.id);
 });
+//handling incoming stroke drawing events from other clients
+socket.on('stroke:draw', (stroke) => {
+    //here we store the remote stroke in the history and render it on the canvas
+    addStrokeToHistory(stroke)//this is used to keep the local stroke history updated with remote strokes
+    //rendering the remote stroke on the canvas
+  renderRemoteStroke(stroke);
+});
 
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
